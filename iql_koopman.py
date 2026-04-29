f8ea58362121e7c5c9a00443062a8a408fcd7895
name <email>
2026-04-29 16:48:41 +0900

    lqr prior

--- iql_koopman.py
+++ iql_koopman.py
@@ -40,7 +40,7 @@ from collections import deque
 from models.koopman_cvae import KoopmanCVAE
 from models.losses import symexp
 from data.extract_skill_label import load_x_sequences
-from lqr_planner import (
+from lqr_koopman import (
     KODAQLQRPlanner, LQRConfig,
     load_kitchen_episodes, obs_to_x_goal,
     X_DQ_START, X_DQ_END, X_DP_START, X_DP_END,
